c88857fac0b02cc7757a627d9f2f5d87ea038458
Franzi-sys <ct26m019@technikum-wien.at>
2026-09-20 18:00:16 +0200

    Add counter component and remove todo components

---
+++ todo-app/src/components/Counter.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { ref , computed } from 'vue'
+
+const count = ref(0)
+const doubleCount = computed(() => count.value * 2) 
+
+function increment() {
+  count.value++
+}
+
+function decrement() {
+  count.value--
+}
+</script>
+
+<template>
+  <div>
+    <h1>Counter</h1>
+    <p>Zählerstand: {{ count }}</p>
+    <p>Doppelt so viel: {{ doubleCount }}</p>
+    <button @click="increment">Hochzählen</button>
+    <button @click="decrement">Runterzählen</button>
+  </div>
+</template>
